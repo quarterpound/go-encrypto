b6544e25fd5d1c085d826e2d91c7cae2b2f468fc
Ali Gasimzade <gasimzadeali@gmail.com>
2020-08-14 23:09:56 +0400

    first commit

---
+++ util.go
@@ -0,0 +1,116 @@
+package encrypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
+	"io"
+)
+
+func EncryptAES(key []byte, plaintext string) (string, error) {
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	out := make([]byte, len(plaintext))
+
+	c.Encrypt(out, []byte(plaintext))
+	return hex.EncodeToString(out), nil
+}
+
+func DecryptAES(key []byte, ct string) (string, error) {
+	ciphertext, _ := hex.DecodeString(ct)
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	pt := make([]byte, len(ciphertext))
+	c.Decrypt(pt, ciphertext)
+
+	return string(pt[:]), nil
+}
+
+
+// Taken from https://github.com/gtank/cryptopasta/blob/master/encrypt.go
+
+// Generates a new random encryption key
+// of size 32
+func NewEncryptionKey() *[32]byte {
+	key := [32]byte{}
+	_, err := io.ReadFull(rand.Reader, key[:])
+	if err != nil {
+		panic(err)
+	}
+	return &key
+}
+
+// Converts plaintext string to appropriate 32 bytea key
+func ConvertEncryptionKey(key string) ([]byte, error) {
+	if len(key) == 0 {
+		return nil, errors.New("no input supplied")
+	}
+
+	hasher := sha256.New()
+	hasher.Write([]byte(key))
+
+
+	stringToSHA256 := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+
+	//Cut the length down to 32 bytes and return.
+	return []byte(stringToSHA256[:32]), nil
+}
+
+// Encrypt encrypts data using 256-bit AES-GCM.  This both hides the content of
+// the data and provides a check that it hasn't been altered. Output takes the
+// form nonce|ciphertext|tag where '|' indicates concatenation.
+func Encrypt(key *[]byte, plaintext []byte) (ciphertext []byte, err error) {
+	block, err := aes.NewCipher(*key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+// Decrypt decrypts data using 256-bit AES-GCM.  This both hides the content of
+// the data and provides a check that it hasn't been altered. Expects input
+// form nonce|ciphertext|tag where '|' indicates concatenation.
+func Decrypt(key *[]byte, ciphertext []byte) (plaintext []byte, err error) {
+	block, err := aes.NewCipher(*key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < gcm.NonceSize() {
+		return nil, errors.New("malformed ciphertext")
+	}
+
+	return gcm.Open(nil,
+		ciphertext[:gcm.NonceSize()],
+		ciphertext[gcm.NonceSize():],
+		nil,
+	)
+}
